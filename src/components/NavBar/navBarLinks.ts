7f53d5881ad85cc18f7705f677552cc6d472e611
Hafiz Bilal Ahmad <gondal.hafizbilal@gmail.com>
2024-05-09 12:16:28 +0500

    built nav bar component.

---
+++ src/components/NavBar/navBarLinks.ts
@@ -0,0 +1,10 @@
+type NavLinks = {
+  id: number
+  label: string
+  href: string
+}
+
+export const navBarLinks: NavLinks[] = [
+  { id: 1, label: 'Dashboard', href: '/' },
+  { id: 2, label: 'Issues', href: '/issues' },
+]
