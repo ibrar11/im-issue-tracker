425dec185366bf6e2085269a42035f1d4115983a
Ibrar Munawar <ibrarmunawr0@gmail.com>
2024-05-13 16:33:33 +0500

    handle form errors.

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -3,15 +3,22 @@ import prisma from '../../../../prisma/client'
 import { z } from 'zod'
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z
+    .string()
+    .min(1, 'Title cannot be empty')
+    .max(255, 'Title cannot be more than 255 characters.'),
+  description: z
+    .string()
+    .min(1, 'Description cannot be empty')
 })
 
 export const POST = async (request: NextRequest) => {
   const body = await request.json()
   const validateIssueSchema = createIssueSchema.safeParse(body)
   if (!validateIssueSchema.success) {
-    return NextResponse.json(validateIssueSchema.error.errors, { status: 400 })
+    return NextResponse.json(validateIssueSchema.error.format(), {
+      status: 400,
+    })
   }
   const newIssue = await prisma.issue.create({
     data: {
