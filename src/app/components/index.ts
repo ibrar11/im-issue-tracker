d37e11ce5b0f73a9bc871536b87d54b2c388f054
Ibrar Munawar <ibrarmunawr0@gmail.com>
2024-05-14 17:14:21 +0500

    create the spinner component.

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,2 +1,3 @@
 export { default as NavBar } from '@/app/components/NavBar/Navbar'
 export { default as ErrorMessage } from '@/app/components/ErrorMessage/ErrorMessage'
+export { default as Spinner } from '@/app/components/Spinner/Spinner'
