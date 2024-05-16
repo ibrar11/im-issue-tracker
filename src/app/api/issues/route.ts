ae96b7c8324b69a320f722322c729fb58f70d5d4
Ibrar Munawar <ibrarmunawr0@gmail.com>
2024-05-16 18:06:04 +0500

    show the issues list.

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -18,3 +18,15 @@ export const POST = async (request: NextRequest) => {
   })
   return NextResponse.json(newIssue, { status: 201 })
 }
+
+export const GET = async () => {
+  try {
+    const issues = await prisma.issue.findMany()
+    if (!issues.length) {
+      return NextResponse.json('No record to show', { status: 404 })
+    }
+    return NextResponse.json(issues, { status: 200 })
+  } catch (error) {
+    return NextResponse.json(error, { status: 500 })
+  }
+}
