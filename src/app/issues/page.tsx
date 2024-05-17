b681151025077c0aa46c95374865f3c820849163
Ibrar Munawar <ibrarmunawr0@gmail.com>
2024-05-17 15:32:28 +0500

    add loading skeletons.

---
+++ src/app/issues/page.tsx
@@ -0,0 +1,84 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { Table } from '@radix-ui/themes'
+import axios from 'axios'
+import { IssueStatusBadge, Spinner } from '@/app/components'
+import { Status } from '@prisma/client'
+import AddIssueButton from './AddIssueButton'
+
+type issue = {
+  id: number
+  title: string
+  description: string
+  status: Status
+  createdAt: string
+  updatedAt: string
+}
+
+const IssuesPage = () => {
+  const [issues, setIssues] = useState<issue[]>()
+
+  const getIssues = async () => {
+    try {
+      const response = await axios.get('/api/issues')
+      response?.data.map((issue: any) => {
+        issue.createdAt = new Date(issue.createdAt).toDateString()
+        issue.updatedAt = new Date(issue.updatedAt).toDateString()
+      })
+      setIssues(response?.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getIssues()
+  }, [])
+
+  return (
+    <div>
+      <AddIssueButton />
+      {/* {issues ? ( */}
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues?.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+      {/* ) : (
+        <div className="flex items-center justify-center gap-x-2">
+          <Spinner styles="!text-blue-600 !size-6">
+            <p className="text-2xl font-normal text-blue-600">Loading...</p>
+          </Spinner>
+        </div>
+      )} */}
+    </div>
+  )
+}
+
+export default IssuesPage
