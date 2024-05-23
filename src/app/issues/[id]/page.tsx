e1d5a6619286d369b22b0d9c1a5c7dcc02027d3e
Ibrar Munawar <ibrarmunawr0@gmail.com>
2024-05-23 13:13:55 +0500

    add styles to issue details page.

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,5 +1,7 @@
 'use client'
-import { Issue } from '@prisma/client'
+import { IssueStatusBadge } from '@/app/components'
+import { Issue, Status } from '@prisma/client'
+import { Heading, Flex, Card, Text } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
@@ -16,7 +18,10 @@ const IssueDetailPage = ({ params }: Props) => {
   const findIssue = async (id: string) => {
     try {
       const response = await axios.get(`/api/issues/${id}`)
-      setIssue(response.data?.issue)
+      setIssue({
+        ...response.data?.issue,
+        createdAt: new Date(response.data?.issue?.createdAt),
+      })
     } catch (error: any) {
       if (error?.response?.status === 404 || error?.response?.status === 400) {
         router.push(`/not-found?prevPath=${prevPath}`)
@@ -31,10 +36,14 @@ const IssueDetailPage = ({ params }: Props) => {
 
   return (
     <div>
-      <p>{issue?.title}</p>
-      <p>{issue?.description}</p>
-      <p>{issue?.status}</p>
-      {/* <p>{issue?.createdAt.toDateString()}</p> */}
+      <Heading>{issue?.title}</Heading>
+      <Flex className="space-x-3" my="2">
+        <IssueStatusBadge status={issue?.status} />
+        <Text>{issue?.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        <p>{issue?.description}</p>
+      </Card>
     </div>
   )
 }
