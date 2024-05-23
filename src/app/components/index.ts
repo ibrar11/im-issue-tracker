d380feaaccb95e10e8f8bae44bdadee3de5bebc8
Ibrar Munawar <ibrarmunawr0@gmail.com>
2024-05-23 16:15:21 +0500

    oragnize import statements.

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -3,3 +3,4 @@ export { default as ErrorMessage } from '@/app/components/ErrorMessage/ErrorMess
 export { default as Spinner } from '@/app/components/Spinner/Spinner'
 export { default as IssueStatusBadge } from '@/app/components/IssueStatusBadge/IssueStatusBadge'
 export { default as Link } from '@/app/components/Link/Link'
+export { default as Skeleton } from '@/app/components/Skeleton/Skeleton'
