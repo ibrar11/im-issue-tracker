4b4db2311ae6d3cfe8d12244c1bb1d58afa5ba08
Ibrar Munawar <ibrarmunawr0@gmail.com>
2024-05-25 20:15:00 +0500

    integrated edit api.

--- src/app/issues/[id]/edit/page.tsx
+++ src/app/issues/[id]/edit/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import IssueForm from '../../_components/IssueForm'
 
-const EditIssuePage = () => {
-  return <IssueForm />
+const EditIssuePage = ({ params }: { params: { id: string } }) => {
+  return <IssueForm id={params.id} />
 }
 
 export default EditIssuePage
